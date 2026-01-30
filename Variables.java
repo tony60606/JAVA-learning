ec3287c9020460736f2d0b0d7be8ad25bf939c64
tony <ttoonnyy9@gmail.com>
2026-01-30 17:03:18 +0800

    Add Variables

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+  public static void main(String[] args) {
+    String studentName = "Tony Cheng" ;
+    int studentID = 16 ; 
+    int studentAge = 30 ;
+    float studentFee = 127.25f ;
+    char studentGrade = 'A' ;
+    int studentClass = 303 ;
+    System .out.println("student name" + studentName) ; 
+    System .out.println("student ID" + studentID) ; 
+    System .out.println("student age" + studentAge) ; 
+    System .out.println("student fee" + studentFee) ; 
+    System .out.println("student grade" + studentGrade) ; 
+    System .out.println("student class" + studentClass) ; 
+  }
+}

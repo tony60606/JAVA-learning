663ade6f9a7317dc50c5be1fa0a306c7bed6c42c
tony <ttoonnyy9@gmail.com>
2026-01-30 17:15:58 +0800

    Add Area

---
+++ Area.java
@@ -0,0 +1,11 @@
+public class Area {
+  public static void main(String[] args) {
+    int length = 10;
+    int width = 30;
+    int area;
+    area = length * width;
+    System.out.println("Length is : " + length);
+    System.out.println("Width is : " + width);
+    System.out.println("Area is : " + area);
+  }
+}

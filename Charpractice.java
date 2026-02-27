ab54926284a8592e31f5fd09ce4f3f69a52f8751
tony <ttoonnyy9@gmail.com>
2026-02-27 14:53:34 +0800

    Char

---
+++ Charpractice.java
@@ -0,0 +1,12 @@
+public class Charpractice {
+  public static void main (String [ ] args ) {
+    String 產品名稱 = "電腦" ;
+    int 數量 = 15 ;
+    float 單價 = 31.31f ;
+    float 金額 = 數量 * 單價 ;
+    char 幣別 = '$' ;
+    System.out.println(產品名稱) ;
+    System.out.println(幣別+""+單價) ;
+    System.out.println(幣別+""+金額) ;
+  }
+}

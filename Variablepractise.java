11f810f91687dfe7138720e176cc89b8ab0e5f47
tony <ttoonnyy9@gmail.com>
2026-02-27 12:44:42 +0800

    Add Variable practice

---
+++ Variablepractise.java
@@ -0,0 +1,8 @@
+public class Variablepractise {
+  public static void main (String [ ] args) {
+    String name = "Tony" ;
+    int age = 30 ;
+    System.out.println(name) ; 
+    System.out.println(age) ;
+  }
+}

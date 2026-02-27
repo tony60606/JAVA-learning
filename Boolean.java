10031dc52fd744b77f0bdc13c19cb59f2d047976
tony <ttoonnyy9@gmail.com>
2026-02-27 12:57:58 +0800

    Boolean

---
+++ Boolean.java
@@ -0,0 +1,10 @@
+public class Boolean {
+  public static void main (String [ ] args ) {
+    boolean Tony = true ;
+    boolean Kirby = true ;
+    boolean Buddy = false ;
+    System.out.println(Tony) ;
+    System.out.println(Kirby) ;
+    System.out.println(Buddy) ;
+  }
+}

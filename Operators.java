d488fc2e79e3f3035762cd7075ac9d25fff5f446
tony <ttoonnyy9@gmail.com>
2026-03-02 20:05:42 +0800

    Operators

---
+++ Operators.java
@@ -0,0 +1,19 @@
+public class Operators {
+  public static void main (String [ ] args) {
+    int A = 55 ; 
+    int B = 54 ;
+    System.out.println(A+B) ;
+    System.out.println(A-B) ;
+    System.out.println(A*B) ;
+    System.out.println(A/B) ;
+    System.out.println(A%B) ;
+    System.out.println(A++) ;
+    System.out.println(A) ;
+    System.out.println(++A) ;
+    System.out.println(A) ;
+    System.out.println(B--) ;
+    System.out.println(B) ;
+    System.out.println(--B) ;
+    System.out.println(B) ;
+  }
+}

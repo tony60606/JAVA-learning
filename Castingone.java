5941fbad6d857b93b9bd4e0ed03f57722c4750f0
tony <ttoonnyy9@gmail.com>
2026-03-02 18:50:06 +0800

    Casting

---
+++ Castingone.java
@@ -0,0 +1,9 @@
+public class Castingone {
+  public static void main (String [ ] args) {
+    int Globalboxoffice = 500 ;
+    int Area = 42 ;
+    double boxoffice = (double) Globalboxoffice / Area * 100.0d ;
+    System.out.println(Globalboxoffice) ;
+    System.out.println(boxoffice) ;
+  }
+}
---
+++ Castingtwo.java
@@ -0,0 +1,8 @@
+public class Castingtwo {
+  public static void main (String [ ] args) {
+    double pi = 3.1415926d ;
+    int Int = (int) pi ; 
+    System.out.println(pi) ;
+    System.out.println(Int) ;
+  }
+}

68bda0a14972f407f77fae0e5fa23810b8e64f4c
tony <ttoonnyy9@gmail.com>
2026-03-02 20:11:45 +0800

    Operatorstwo

---
+++ Operatorstwo.java
@@ -0,0 +1,10 @@
+public class Operatorstwo {
+  public static void main (String [ ] args) {
+    int room = 0 ;
+    room++ ;
+    room++ ;
+    System.out.println(room) ;
+    room-- ;
+    System.out.println(room) ;
+  }
+}

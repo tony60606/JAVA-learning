0a6ba1de61b0424958983462f9cebed5670e9be9
tony <ttoonnyy9@gmail.com>
2026-03-06 16:51:34 +0800

    Math &String

---
+++ Mathcass.java
@@ -0,0 +1,19 @@
+public class Mathcass {
+  public static void main (String [] args) {
+    int X = 50 ; 
+    int Y = 100 ; 
+    double Z = 3.5 ; 
+    int x = (int) Math.random() ;
+    int y = (int) (Math.random()*1001) ;
+    System.out.println(Math.max(X,Y)) ; 
+    System.out.println(Math.min(X,Y)) ; 
+    System.out.println(Math.sqrt(X)) ; 
+    System.out.println(Math.abs(X)) ; 
+    System.out.println(Math.pow(X,Y)) ; 
+    System.out.println(Math.round(Z)) ; 
+    System.out.println(Math.ceil(Z)) ; 
+    System.out.println(Math.floor(Z)) ; 
+    System.out.println(x) ; 
+    System.out.println(y) ;
+  }
+} 
---
+++ Stringcass.java
@@ -0,0 +1,9 @@
+public class Stringcass {
+  public static void main (String [] args) {
+    String 姓名 = "王大明" ;
+    int 年齡 = 30 ;
+    int 身高 = 175 ;
+    int 體重 = 65 ;
+    System.out.println("學生你好，你的姓名是" + "\"" + 姓名 + "\"" + "\n你的年齡是" + 年齡 + "歲"+ "\n你的身高是" + 身高 + "公分" + "\n你的體重是" + 體重 + "公斤") ;
+  }
+}
